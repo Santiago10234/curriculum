5e0320a239eb1ffe367ea12cb94fd2bd0622e33b
santiago10234 <ariza.ps4@gmail.com>
2024-05-01 09:37:24 -0600

    Creacion de funciones a los icono de hobby

---
+++ index.js
@@ -0,0 +1,31 @@
+let imagen = document.getElementById("imagen").onclick = function () {
+    correr();
+};
+
+function correr(){
+    alert("Salgo a correr todos los dias por lo menos 30 min.")
+}
+
+let imagen2 = document.getElementById("imagen2").onclick = function () {
+    viajar();
+};
+
+function viajar(){
+    alert("Me encanta viajar y conocer lugares nuevos.")
+}
+
+let imagen3 = document.getElementById("imagen3").onclick = function () {
+    tv();
+};
+
+function tv(){
+    alert("Me fascina ver peliculas, mi categoria favorita es la comedia.")
+}
+
+let imagen4 = document.getElementById("imagen4").onclick = function () {
+    musica();
+};
+
+function musica(){
+    alert("Me gusta todo tipo de musica, pero de preferencia hip hop.")
+}
